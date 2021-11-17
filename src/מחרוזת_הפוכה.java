5c3fb79d2542936214f0f9fc706e53be806c05e5
Arie Levy <arielevy17@gmail.com>
2021-11-17 21:40:26 +0200

    Initial commit

---
+++ src/מחרוזת_הפוכה.java
@@ -0,0 +1,19 @@
+public class מחרוזת_הפוכה {
+    public static String revers(String yourWord){
+        String newWord = " ";
+        for (int i = yourWord.length()-1; i >= 0; i--){
+            newWord += yourWord.charAt(i);
+
+            }
+        return newWord;
+
+        }
+
+    public static void main(String[] args) {
+        System.out.println(revers("arie"));
+    }
+
+    }
+
+
+
